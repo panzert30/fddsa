46341d01b7c0fa0bd9671f3120790588970e4040
panzert30 <55293857+panzert30@users.noreply.github.com>
2019-10-25 14:28:17 -0400

    ertj

---
+++ game.py
@@ -0,0 +1,18 @@
+from random import randint
+
+aaaaaa=["rock","scissors","paper"]
+
+computer=aaaaaa[randint(0,2)]
+player= input("1,2,3 pick one") 
+player=False
+
+while player is False:
+	
+	pass
+
+
+
+
+print(player,computer)
+
+
